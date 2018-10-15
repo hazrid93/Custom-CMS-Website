396ea2cb5552bb472034171a172f1bfdfc130d8b
Hazrid Azad <hazrid93@hotmail.com>
2018-10-15 20:15:24 +0800

    refactor admin page

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,15 +1,11 @@
            <div class="col-md-4">
                
-                <?php
-                   if(isset($_POST['submit'])){
-                      $search = $_POST['search'];
-                   }
-               ?>
+
 
                 <!-- Blog Search Well -->
                 <div class="well">
                     <h4>Blog Search</h4>
-                    <form action="" method="post">
+                    <form action="search.php" method="post">
                     <div class="input-group">
                         <input name="search" type="text" class="form-control">
                         <span class="input-group-btn">
@@ -24,42 +20,56 @@
 
                 <!-- Blog Categories Well -->
                 <div class="well">
-                    <h4>Blog Categories</h4>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
-                    </div>
+                    <?php
+                                           
+                       $query = "SELECT * FROM categories";
+                       $search_categories = mysqli_query($connection,$query);
+
+                       if(!$search_categories){
+                           die("QUERY FAILED" . mysqli_error);
+                       } else {
+                           ?>
+                               
+                               
+                            <h4>Blog Categories</h4>
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <ul class="list-unstyled">
+                                        <?php
+                                            while($row = mysqli_fetch_assoc($search_categories)){
+                                            $cat_title = $row['cat_title'];
+
+                                            echo "<li><a href='#'>{$cat_title}</a></li>";
+                                                }
+                                            ?>
+                                    </ul>
+                                </div>
+                                <!-- /.col-lg-6 -->
+                              <!--  <div class="col-lg-6">
+                                    <ul class="list-unstyled">
+                                        <li><a href="#"><?php echo $cat_title ?></a>
+                                        </li>
+                                        <li><a href="#"><?php echo $cat_title ?></a>
+                                        </li>
+                                        <li><a href="#"><?php echo $cat_title ?></a>
+                                        </li>
+                                        <li><a href="#"><?php echo $cat_title ?></a>
+                                        </li>
+                                    </ul>
+                                </div> -->
+                                <!-- /.col-lg-6 -->
+                            </div>
+
+                    <?php
+                           
+                       }
+                    ?>
+                    
+
                     <!-- /.row -->
                 </div>
 
                 <!-- Side Widget Well -->
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
+                <?php include "widgets.php" ?>
 
             </div>
